3eb8a35dc174ebb480dd402741aa6683ee78193c
Brandon Van Blarcum <41463636+Drofal980@users.noreply.github.com>
2022-01-12 09:24:05 -0700

    Initial Format

---
+++ PythonFormat.py
@@ -0,0 +1,21 @@
+#IMPORTS
+
+#GLOBAL VARIABLES
+
+#MAIN FUNCTION
+def main():
+    """[Function purpose here]
+
+    Parameters
+        variable: Purpose
+
+    Returns
+        if applicable
+    """
+    pass
+
+#OTHER FUNCTIONS
+
+#INITIALIZES MAIN
+if __name__ == "__main__":
+  main() 
